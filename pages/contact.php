0c345d0d074debc8f4e4e95977d4f750a48fac96
Neordan <83787849+Neordan@users.noreply.github.com>
2023-06-06 13:54:29 +0200

    Ajout de messages d'erreurs dans la page contact

--- pages/contact.php
+++ pages/contact.php
@@ -1,90 +1,87 @@
 <?php
 
+
 $page_title = "Rendez-vous";
 require "../core/header.php";
 require "../core/config.php";
-
-
 
 // Traitement des dates de réservations
 require "../dateProcessing.php";
 
 $slots = ['10:00', '13:30', '16:30'];
 
+// Obtention de la date de début de la semaine précédente
 $debutSemainePrecedente = (new DateTime($calendrier["lundi"]->format("Y-m-d")))->modify("previous monday");
+
+// Obtention de la date de début de la semaine suivante
 $debutSemaineSuivante = (new DateTime($calendrier["dimanche"]->format("Y-m-d")))->modify("next monday");
 
 if (isset($_POST['date-resa']) && !is_array($_POST['date-resa'])) {
+    // Si la date et l'heure sont sélectionnées via le calendrier mobile, les découper en date et heure distinctes
     $_POST['date-resa'] = [substr($_POST['date-resa'], 0, 10), substr($_POST['date-resa'], 11)];
 }
+
+$errors = []; // Tableau pour stocker les erreurs
 
 // Insertion d'une réservation
 if (!empty($_POST)) {
     // Vérification des champs obligatoires
-if (isset($_POST["date-resa"]) && isset($_SESSION["utilisateur"]["id"]) && !empty($_POST['prestation']) && !empty($_FILES['inspiration']['name'])) {
-    // Si la date et l'heure sont sélectionnées via le calendrier mobile, les utiliser
-    if (is_array($_POST["date-resa"]) && count($_POST["date-resa"]) > 1) {
-        $rdv = $_POST["date-resa"][0] . " " . $_POST["date-resa"][1];
-    } else {
-        echo "vous n'avez choisi qu'un seul champ";
-    }
-    
-    // Enregistrement du fichier d'inspiration
-    if (!empty($_FILES['inspiration']['name'])) {
-        $inspirationFileName = $_FILES['inspiration']['name'];
-        $inspirationFilePath = "../assets/img/clients/" . $inspirationFileName;
-        if (move_uploaded_file($_FILES['inspiration']['tmp_name'], $inspirationFilePath)) {
-            $inspiration = "assets/img/clients/" . $inspirationFileName;
+    if (isset($_POST["date-resa"]) && isset($_SESSION["utilisateur"]["id"]) && !empty($_POST['prestation']) && !empty($_FILES['inspiration']['name'])) {
+        // Si la date et l'heure sont sélectionnées via le calendrier mobile, les utiliser
+        if (is_array($_POST["date-resa"]) && count($_POST["date-resa"]) > 1) {
+            $rdv = $_POST["date-resa"][0] . " " . $_POST["date-resa"][1];
         } else {
-            echo "Une erreur s'est produite lors de l'enregistrement du fichier d'inspiration.";
-            exit;
+            $errors[] = "Veuillez sélectionner une date et une heure pour le rendez-vous.";
         }
-    }
-    
-    // Enregistrement du fichier d'ongles actuels
-    if (!empty($_FILES['ongle_actuel']['name'])) {
-        $ongleActuelFileName = $_FILES['ongle_actuel']['name'];
-        $ongleActuelFilePath = "../assets/img/clients/" . $ongleActuelFileName;
-        if (move_uploaded_file($_FILES['ongle_actuel']['tmp_name'], $ongleActuelFilePath)) {
-            $ongle_actuel = "assets/img/clients/" . $ongleActuelFileName;
-        } else {
-            echo "Une erreur s'est produite lors de l'enregistrement du fichier d'ongles actuels.";
-            exit;
-        }
-    }
 
-        // Requête SQL
-        $sql = "INSERT INTO rdv (id_utilisateur, jour_heure, inspiration, ongle_actuel, prestation, message) 
+        // Enregistrement du fichier d'inspiration
+        if (!empty($_FILES['inspiration']['name'])) {
+            $inspirationFileName = $_FILES['inspiration']['name'];
+            $inspirationFilePath = "../assets/img/clients/" . $inspirationFileName;
+            if (move_uploaded_file($_FILES['inspiration']['tmp_name'], $inspirationFilePath)) {
+                $inspiration = "assets/img/clients/" . $inspirationFileName;
+            } else {
+                $errors[] = "Une erreur s'est produite lors de l'enregistrement du fichier d'inspiration.";
+            }
+        } else {
+            $errors[] = "Veuillez sélectionner un fichier d'inspiration.";
+        }
+
+        // Enregistrement du fichier d'ongles actuels
+        if (!empty($_FILES['ongle_actuel']['name'])) {
+            $ongleActuelFileName = $_FILES['ongle_actuel']['name'];
+            $ongleActuelFilePath = "../assets/img/clients/" . $ongleActuelFileName;
+            if (move_uploaded_file($_FILES['ongle_actuel']['tmp_name'], $ongleActuelFilePath)) {
+                $ongle_actuel = "assets/img/clients/" . $ongleActuelFileName;
+            }
+        }
+        if (empty($errors)) {
+            // Requête SQL pour l'insertion de la réservation
+            $sql = "INSERT INTO rdv (id_utilisateur, jour_heure, inspiration, ongle_actuel, prestation, message) 
                 VALUES (:id, :rdv, :inspiration, :ongle_actuel, :prestation, :message)";
-        $id = $_SESSION["utilisateur"]["id"];
-        $rdv = $_POST["date-resa"][0] . " " . $_POST["date-resa"][1];
-        $prestation = $_POST["prestation"];
-        $inspiration = "assets/img/clients/" . $_FILES['inspiration']['name'];
-        $ongle_actuel = !empty($_FILES['ongle_actuel']['name']) ? "assets/img/clients/" . $_FILES['ongle_actuel']['name'] : null;
-        $message = !empty($_POST['message']) ? htmlspecialchars(trim($_POST['message'])) : null;
+            $id = $_SESSION["utilisateur"]["id"];
+            $rdv = $_POST["date-resa"][0] . " " . $_POST["date-resa"][1];
+            $prestation = $_POST["prestation"];
+            $inspiration = "assets/img/clients/" . $inspirationFileName;
 
-        // Préparation et exécution de la requête
-        $query = $pdo->prepare($sql);
-        $query->bindParam(":id", $id, PDO::PARAM_STR);
-        $query->bindParam(":rdv", $rdv, PDO::PARAM_STR);
-        $query->bindParam(":inspiration", $inspiration, PDO::PARAM_STR);
-        $query->bindParam(":ongle_actuel", $ongle_actuel, PDO::PARAM_STR);
-        $query->bindParam(":prestation", $prestation, PDO::PARAM_STR);
-        $query->bindParam(":message", $message, PDO::PARAM_STR);
+            // Préparation de la requête et exécution
+            $stmt = $pdo->prepare($sql);
+            $stmt->bindParam(':id', $id);
+            $stmt->bindParam(':rdv', $rdv);
+            $stmt->bindParam(':inspiration', $inspiration);
+            $stmt->bindParam(':ongle_actuel', $ongle_actuel);
+            $stmt->bindParam(':prestation', $prestation);
+            $stmt->bindParam(':message', $_POST["message"]);
+            $stmt->execute();
 
-        if ($query->execute()) {
-            $_SESSION['utilisateur']['id'] = $id;
-            // Rediriger vers la page d'accueil
-            $_SESSION['rdv']['jour_heure'] = $rdv;
+            // Confirmation de la réservation
             header('Location: ./profil.php');
-        } else {
-            echo "Erreur 1.";
+            exit;
         }
     } else {
-        echo "Vous avez déjà pris rdv";
+        $errors[] = "Veuillez remplir tous les champs obligatoires.";
     }
 }
-
 
 // Récupération des prestations de la base de données (ENUM)
 
@@ -105,6 +102,16 @@ $prestations = $matches[1];
 
 <h2>contact</h2>
 <form class="contact" method="post" enctype="multipart/form-data">
+    <!-- Affichage des erreurs -->
+    <?php if (!empty($errors)) : ?>
+        <div class="error-container">
+            <ul class="error-list">
+                <?php foreach ($errors as $error) : ?>
+                    <li><?= $error; ?></li>
+                <?php endforeach; ?>
+            </ul>
+        </div>
+    <?php endif; ?>
     <input type="hidden" name="id_utilisateur" value="<?= $_SESSION['utilisateur']['id'] ?>">
     <div class="mobile-calendar">
         <label class="labelMobile" for="mobile-date">Choisir la date :</label>
@@ -116,7 +123,7 @@ $prestations = $matches[1];
             <?php endforeach; ?>
         </select>
     </div>
-    <div class="agenda">
+    <div class="desktop-calendar">
 
         <!-- Navigation de l'agenda -->
         <div class="agenda-nav">
@@ -214,7 +221,7 @@ $prestations = $matches[1];
     </div>
     <button class="formulaire"><i class="fa-solid fa-check"></i></button>
 
-<?php 
+    <?php
 
-require_once "../core/footer.php";
-?>
+    require_once "../core/footer.php";
+    ?>
