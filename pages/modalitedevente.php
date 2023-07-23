9a1f1d95384c94be4a370462d220bf8836df2253
Neordan <83787849+Neordan@users.noreply.github.com>
2023-07-24 00:53:55 +0200

    définition du style de la page modalite de vente

--- pages/modalitedevente.php
+++ pages/modalitedevente.php
@@ -7,24 +7,31 @@ require_once "../core/header.php";
 
 <h2>Modalité de vente</h2>
 
-<p class="modalite">Bienvenue sur notre site de prothésie ongulaire ! Je suis ravie de t'offrir mes services professionnels pour sublimer tes ongles et t'aider à révéler ta beauté unique. Avant de procéder à ta commande, prends connaissance de mes modalités de vente pour une expérience de qualité et satisfaisante.
+<p class="modalite">Bienvenue sur mon site de prothésie ongulaire ! Je suis ravie de t'offrir mes services professionnels pour sublimer tes ongles et t'aider à révéler ta beauté unique. Avant de procéder à ta commande, prends connaissance de mes modalités de vente pour une expérience de qualité et satisfaisante.
 <br>
-<span>Prestations et Services:</span>
+<br>
+<span>Prestations et Services:</span><br>
 Je propose une gamme variée de prestations de prothésie ongulaire, allant des poses de vernis classiques aux créations artistiques sur ongles en gel ou en résine. Chacun de mes services est réalisé avec passion et savoir-faire, en utilisant des produits de haute qualité pour assurer un résultat impeccable.
 <br>
-<span>Prise de Rendez-vous:</span>
-La prise de rendez-vous peut être effectuée en ligne via mon site ou par téléphone. Je te conseille de réserver ton créneau à l'avance pour t'assurer d'avoir un horaire qui te convient le mieux. En cas d'empêchement, préviens-moi au moins 24 heures à l'avance afin de permettre à d'autres clientes de profiter de mes services.
 <br>
-<span>Paiement et Tarifs:</span>
-Mes tarifs varient en fonction des prestations choisies. Tous les prix sont clairement indiqués sur mon site. Le paiement peut être effectué en ligne au moment de la réservation ou directement sur place, en espèces ou par carte bancaire. Je te communiquerai également tout frais supplémentaire, comme les nail arts spéciaux ou les déplacements à domicile, en toute transparence.
+<span>Prise de Rendez-vous:</span><br>
+La prise de rendez-vous peut être effectuée en ligne via mon site ou sur mon instagram. Je te conseille de réserver ton créneau à l'avance pour t'assurer d'avoir un horaire qui te convient le mieux. En cas d'empêchement, préviens-moi au moins 24 heures à l'avance afin de permettre à d'autres clientes de profiter de mes services.
 <br>
-<span>Politique d'Hygiène et de Santé:</span>
+<br>
+<span>Paiement et Tarifs:</span><br>
+Mes tarifs varient en fonction des prestations choisies. Tous les prix sont clairement indiqués sur mon site. Le paiement de l'acompte peut être effectué en ligne via PayPal ou Paylib au moment de la réservation. Le reste du paiement sera effectué sur place, en espèces, le jour de ton rendez-vous.
+<br>
+<br>
+<span>Politique d'Hygiène et de Santé:</span><br>
 Ta santé et ta sécurité sont ma priorité. Je respecte des normes d'hygiène strictes en désinfectant tous mes outils et en utilisant des équipements jetables lorsque nécessaire. En cas d'infection ou de problèmes cutanés, n'hésite pas à m'en informer avant ta séance, afin que j'adapte mes prestations en conséquence.
 <br>
-<span>Annulation et Remboursement:</span>
-En cas d'annulation de ta part, préviens-moi au moins 24 heures avant ton rendez-vous pour un remboursement intégral. Pour toute annulation tardive ou absence, des frais peuvent s'appliquer. Si un problème survient lors de la prestation, fais-le moi savoir immédiatement, et je ferai de mon mieux pour trouver une solution satisfaisante.
 <br>
-Je suis convaincue que tu apprécieras mes prestations de prothésie ongulaire et que tu repartiras avec des ongles sublimes et un sourire éclatant. N'hésite pas à me contacter si tu as des questions ou des besoins spécifiques. Je suis impatiente de t'accueillir dans mon salon et de t'offrir une expérience exceptionnelle et resplendissante !</p>
+<span>Annulation et Remboursement:</span><br>
+En cas d'annulation de ta part, préviens-moi au moins 72 heures avant ton rendez-vous pour un remboursement intégral de l'acompte. Pour toute annulation tardive ou absence, l'acompte de 10€ ne sera pas remboursé. Si un problème survient lors de la prestation, fais-le moi savoir immédiatement, et je ferai de mon mieux pour trouver une solution satisfaisante.
+<br>
+<br>
+<span>
+Je suis convaincue que tu apprécieras mes prestations de prothésie ongulaire et que tu repartiras avec des ongles sublimes et un sourire éclatant. N'hésite pas à me contacter si tu as des questions ou des besoins spécifiques. Je suis impatiente de t'accueillir dans mon salon et de t'offrir une expérience exceptionnelle et resplendissante !</span></p>
 
 <?php
 require_once "../core/footer.php"
