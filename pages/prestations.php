49cb1ede5a961d6d7cc803dfe8c73236efae82a1
Neordan <83787849+Neordan@users.noreply.github.com>
2023-07-30 23:41:51 +0200

    Amélioration des textes pour les prestations

--- pages/prestations.php
+++ pages/prestations.php
@@ -17,26 +17,23 @@ Que tu recherches un look discret et naturel ou des ongles audacieux et accroche
 En résumé, le vernis semi-permanent est une technique polyvalente et durable qui offre une longue tenue sans nécessiter de retouches fréquentes pour ceux et celles qui souhaitent une manucure impeccable qui dure plusieurs semaines.') ?>
 
 <?= generatePresta('Remplissage', '../assets/img/Photos/IMG-1431.JPG', 'Le remplissage est une technique utilisée pour entretenir et prolonger la durée de vie d\'une pose. Lorsque les ongles naturels poussent, la matière placée 3 voire 4 semaines plus tôt créer un poids sur le bord libre de l\'ongle, ce qui peut rendre la pose d\'ongles inesthétique et entraîner des risques d\'endommagement de l\'ongle naturel.
-<br> 
+<br><br>
 Le remplissage permet donc de combler cet espace en ajoutant de la matière sur l\'ongle naturel qui a poussé. 
 <br><br>
 Bref, le remplissage en prothésie ongulaire est une technique indispensable pour maintenir la beauté et la solidité d\'une pose.') ?>
 
-<?= generatePresta('Gainage', '../assets/img/Photos/IMG-1432.JPG', 'Le gainage offre également la possibilité de créer des extensions d\'ongles pour ceux qui souhaitent obtenir une longueur supplémentaire. En utilisant des matériaux tels que le gel ou l\'acrylique, les techniciens en prothésie ongulaire peuvent modeler et façonner l\'ongle pour obtenir la forme et la longueur désirées. Cela permet d\'obtenir des ongles longs et élégants, tout en maintenant la solidité et la durabilité.
-<br>
-<br>
-Une autre caractéristique avantageuse du gainage est la possibilité de personnalisation. Les gels et les acryliques utilisés pour le gainage sont disponibles dans une large gamme de couleurs, de finitions et de textures. Cela signifie que vous pouvez créer des looks uniques en utilisant des paillettes, des effets de couleur, des motifs et même des décorations comme des strass ou des stickers.
-<br>
-<br>
-En somme, le gainage est une technique polyvalente en prothésie ongulaire qui renforce les ongles naturels, les protège des dommages et offre la possibilité de créer des extensions d\'ongles personnalisées. Que vous ayez des ongles fragiles, que vous souhaitiez obtenir une longueur supplémentaire ou que vous vouliez simplement embellir vos ongles, le gainage peut être une solution adaptée pour obtenir des ongles forts et esthétiquement plaisants.') ?>
+<?= generatePresta('Gainage', '../assets/img/Photos/IMG-1432.JPG', 'Le gainage offre la possibilité de poser de la matière directement sur l’ongle naturel.
+<br><br> 
+À l’inverse d’un renforcement ici le but n’étant pas de conserver sa longueur naturelle mais de l’utiliser comme extension pour pouvoir le supprimer par la suite afin de ne pas créer de décollements qui peuvent causer infiltrations et casses…
+<br><br> 
+Il est recommandé pour celles et ceux qui veulent des ongles longs sans passer par la pose de chablons à la différence que ce sera possible qu’en plusieurs remplissages.') ?>
 
-<?= generatePresta('Pédicure', '../assets/img/Photos/IMG-1429 (1).JPG', 'La pédicure, un soin incontournable pour des pieds heureux et en bonne santé ! Prendre soin de ses pieds est essentiel pour se sentir bien dans sa peau au quotidien. La pédicure, c\'est bien plus qu\'une simple question d\'esthétique, c\'est aussi une démarche de bien-être. 
-<br>
-<br>
-Lors d\'une séance de pédicure, vos pieds sont choyés, nettoyés et chouchoutés pour éliminer les peaux mortes et les callosités. Vos ongles sont taillés avec précision et mis en forme pour éviter tout inconfort. Enfin, l\'application de vernis à ongles apporte la touche finale pour des pieds élégants et soignés.
-<br>
-<br>
-Outre son aspect esthétique, la pédicure présente de nombreux bienfaits pour la santé de vos pieds. Elle permet de prévenir les infections fongiques et les problèmes de peau liés à l\'humidité excessive. En prenant soin de vos pieds, vous contribuez également à soulager les douleurs éventuelles et à améliorer votre confort général.') ?>
+<?= generatePresta('Pédicure', '../assets/img/Photos/IMG-1429 (1).JPG', 'La pédicure, un soin incontournable pour des pieds heureux et en bonne santé ! 
+<br><br>
+Prendre soin de ses pieds est essentiel pour se sentir bien dans sa peau au quotidien. 
+<br><br>
+Lors d\'une séance de pédicure, tes pieds sont choyés, nettoyés et chouchoutés pour éliminer les peaux mortes et hydrate la peau.
+L\'application de vernis (semi-permanent) apporte la touche finale pour des pieds élégants et soignés durant plus de 4 semaines.') ?>
 
 
 <?php require_once "../core/footer.php"; ?>
